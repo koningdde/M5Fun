e4f0ecb76d72aecf0d0cf0d13ead0a760b34ea32
Danny de Koning <koningdde@gmail.com>
2020-11-25 11:32:30 +0100

    Update stackmaster.ino

--- Jilles_M5_Matrix/stackmaster/stackmaster.ino
+++ Jilles_M5_Matrix/stackmaster/stackmaster.ino
@@ -6,9 +6,9 @@
 
 #define SDA_PIN 21
 #define SCL_PIN 22
-#define I2C_SLAVE_ADDR 17
+#define I2C_SLAVE_ADDR 18
 #define MAX_SLAVE_RESPONSE_LENGTH 64
-int slaveAdres = 1;
+int knownDevices[16];   //Array to store "alive" devices
 
 // the setup routine runs once when M5Stack starts up
 void setup(){
@@ -25,59 +25,71 @@ void setup(){
   Serial.begin(115200);           // start serial for output
   Wire.begin(SDA_PIN, SCL_PIN);   // join i2c bus
   
-  // LCD display
-  M5.Lcd.println("Hello World");
 }
 
 // the loop routine runs over and over again forever
 void loop() {
-  int x = 0;
-  for(int i=0;i<=15;i++) {
+
+  for(int i=0;i<=16;i++) {
     WireSlaveRequest slaveReq(Wire, i+1, MAX_SLAVE_RESPONSE_LENGTH);
     slaveReq.setRetryDelay(5);
     bool success = slaveReq.request();
 
     if (success) 
             {
-                while (1 < slaveReq.available()) {
-                char c = slaveReq.read();       // receive byte as a character
-                if (c == 'A') {adresSet();}
-                M5.Lcd.print(c);                // print the character
+                knownDevices[i] = 1;          //Store the device precense in de array
+                while (1 < slaveReq.available()) 
+                  {
+                  char c = slaveReq.read();       // receive byte as a character
+                    if (c == 'A') 
+                      {
+                      adresSet();
+                      }
+                    M5.Lcd.print(c);                // print the character
                 
-            }
+                    }
             }
             else 
-            {
-            // if something went wrong, print the reason
-            
-            M5.Lcd.print(slaveReq.lastStatusToString());
-            M5.Lcd.print(" Slave number: ");
-            M5.Lcd.print(i + 1);
-            }
+              {
+              // if something went wrong, print the reason
+              knownDevices[i+1] = 0;    //Delete the device from known devices
+              M5.Lcd.print(slaveReq.lastStatusToString());
+              M5.Lcd.print(" Slave number: ");
+              M5.Lcd.print(i + 1);
+              }
             
        M5.Lcd.println("                     ");
   }
- 
-  if(x==0) {M5.Lcd.setCursor(0, 0, 1);}
-  delay(500);
+  
+  for(x=0;x<=15;x++)
+    {
+    M5.Lcd.print(knownDevices[x]);    //Print array contents
+    }
+  
+  M5.Lcd.setCursor(0, 0, 1);
+  delay(10);
 }
 
 void adresSet(){
-        WirePacker packer;
+        for(int x=0; x<=15; x++)
+          {
+            if (knownDevices[x]==0)   //Find first available adres
+              {
+              WirePacker packer;
+              // then add data the same way as you would with Wire
+              packer.write('A');
+              packer.write(x+1);
+              // after adding all data you want to send, close the packet
+              packer.end();
 
-        // then add data the same way as you would with Wire
-        packer.write('A');
-        packer.write(slaveAdres);
-        //packer.write(x);
-
-        // after adding all data you want to send, close the packet
-        packer.end();
-
-        // now transmit the packed data
-        Wire.beginTransmission(16);
-        while (packer.available()) {    // write every packet byte
-            Wire.write(packer.read());
-        }
-        Wire.endTransmission();         // stop transmitting
-slaveAdres +=1;
-}
+              // now transmit the packed data
+              Wire.beginTransmission(16);
+              while (packer.available()) 
+                {    // write every packet byte
+                Wire.write(packer.read());
+                }
+              Wire.endTransmission();         // stop transmitting
+              break; //No need to search further
+              }
+          // Adres is in use, do nothing
+          } // End for loop
