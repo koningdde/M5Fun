30274a5d3ee44e5d2e807c6523baf2732dd92fe0
Danny de Koning <koningdde@gmail.com>
2020-12-04 09:10:51 +0100

    Changes to layout

--- Jilles_M5_Matrix/stackmaster/stackmaster.ino
+++ Jilles_M5_Matrix/stackmaster/stackmaster.ino
@@ -1,35 +1,49 @@
-//29-11-2020
-int picture[1300];
-int colorSaturation = 68;
+//Master for ATOM matrix, run on M5 Core
+//When using 16 atoms make sure to use an 3 amp power supply
+//04-12-2020 https://github.com/koningdde/M5Fun
+//Bitmap structure https://en.wikipedia.org/wiki/BMP_file_format
+//Place only 24 Bit, 20x20 pixel with the .BMP extention on the sd card.
 
+int picture[1300];    //Placeholder for active picture
+int colorSaturation = 68;   //Brightness at startup
+
+//Dependicies
 #include <Arduino.h>
 #include <M5Stack.h>
 #include <Wire.h>
 #include <WireSlaveRequest.h>
 #include <WirePacker.h>
+
 //#define DEBUG
-String fileNames[20];
-int w = 0;
+
+String fileNames[20];   //Placeholder for gilenames
+int w = 0;    //Counter for valid BMP files
+
+//Dependicies
 #include "sdFunctions.h"
 
+//I2C Setup
 #define SDA_PIN 21
 #define SCL_PIN 22
 #define I2C_SLAVE_ADDR 18
 #define MAX_SLAVE_RESPONSE_LENGTH 64
 
+//Picture setup
 int knownDevices[17];   //Array to store "alive" devices
-
 int indexNumber[] = {0,1195,1210,1225,1240,895,910,925,940,595,610,625,640,295,310,325,340};    //Store the first bit for the display
 void sendPicture(int device);
 void sendRandom(int device);
 void changePic();
 int actualPic=0;
-int newPatern[16]; //Look for change in patern
-int numberOfPics = 0; //Number of valid pictures on SD
-void adresSet();
-int currentFile = 255; //Placeholder fot current BMP file
-bool autoChange = false;
+int newPatern[16];    //Look for change in patern
+int numberOfPics = 0;   //Number of valid pictures on SD
 
+//Change slave adres function
+void adresSet();
+
+//Autochange setup
+int currentFile = 255;    //Placeholder fot current BMP file
+bool autoChange = false;
 long previousMillis = 0; 
 long interval = 2000;
 unsigned long currentMillis;
@@ -40,7 +54,7 @@ void setup()
   M5.begin();   // Initialize the M5Stack object
   Serial.begin(115200);   // start serial for output
   Wire.begin(SDA_PIN, SCL_PIN);   // join i2c bus
-  //M5.Lcd.setTextColor(0xffff);
+  
     M5.Lcd.setCursor(35, 220);
     M5.Lcd.print("Change picture ");
     M5.Lcd.setCursor(135, 220);
@@ -50,9 +64,11 @@ void setup()
     M5.Lcd.print(colorSaturation);    
     M5.Lcd.print("  "); 
 
+  //Read the SD card
   listDir(SD, "/", 0);
   Serial.println("Bitmap list");
   
+  //Count the valid files names
   for(int i = 0; i<20; i++)
   {
     if(fileNames[i] > " ")
@@ -60,7 +76,8 @@ void setup()
     numberOfPics+=1;
     Serial.println(fileNames[i]);
     }
-
+    
+  //Show results
   }
     Serial.print(numberOfPics);
     Serial.println(" .BMP Pictures found");  
@@ -71,16 +88,14 @@ void setup()
     M5.Lcd.print("Interval: "); 
     M5.Lcd.print(interval);
     M5.Lcd.print("mS     ");
-   // M5.Lcd.setTextColor(0xff80);
     M5.Lcd.setCursor(80, 180,4);
-    M5.Lcd.print("Jilles M5 Matrix");
-   // M5.Lcd.setTextColor(0xffff); 
-    
+    M5.Lcd.print("Jilles M5 Matrix"); 
     M5.Lcd.setCursor(0, 0, 1);
-    
+  
+  //Load first picture
   changePic();
   
-}
+}//End setup loop
 
 // The actual happening
 void loop() {
@@ -110,7 +125,7 @@ void loop() {
                         }
                       }                  
                   
-                    //None
+                    //Nothing here
                 
                     }
             }
@@ -124,7 +139,7 @@ void loop() {
               }
             
        M5.Lcd.println("                 ");   //Clear line 
-  }
+  }//End for loop
 
 #ifdef DEBUG
   for(int x=0;x<=15;x++)
@@ -138,7 +153,7 @@ void loop() {
     if(autoChange == false){changePic();}
     if(autoChange == true)
       {
-        interval = interval + 2000;
+        interval = interval + 2000;   //Change autochange interval
         M5.Lcd.setCursor(0, 154);
         M5.Lcd.print("Interval: "); 
         M5.Lcd.print(interval);
@@ -148,7 +163,7 @@ void loop() {
 
   if (M5.BtnB.wasPressed())
     {
-    autoChange = !autoChange;
+    autoChange = !autoChange;   //Set autochange on or off
     M5.Lcd.setCursor(35, 220);
     if(autoChange == true){M5.Lcd.print("Change interval ");}
     if(autoChange == false){M5.Lcd.print("Change picture  ");}
@@ -163,9 +178,9 @@ void loop() {
     colorSaturation = colorSaturation - 20;
     if(colorSaturation < 20){colorSaturation = 128;}
     M5.Lcd.setCursor(230, 220);
-    M5.Lcd.print("Bright: ");    //Print array contents
-    M5.Lcd.print(colorSaturation);    //Print array contents
-    M5.Lcd.print("  ");    //Print array contents
+    M5.Lcd.print("Bright: ");    
+    M5.Lcd.print(colorSaturation);    
+    M5.Lcd.print("  ");    
     if (actualPic >0) {actualPic-=1;}
     else {actualPic = numberOfPics-1;}
     changePic();
@@ -197,25 +212,23 @@ void adresSet(){
               packer.end();
 
               // now transmit the packed data
-              Wire.beginTransmission(17);
+              Wire.beginTransmission(17);   //Send back the adres to unit 17
               while (packer.available()) 
                 {    // write every packet byte
                 Wire.write(packer.read());
                 }
               Wire.endTransmission();         // stop transmitting
-              break; //No need to search further
+              break; //No need to search further, we have an valid adres
               }
-          // Adres is in use, do nothing
+          // Adres is in use, do nothing, search for next adres
           } // End for loop
-}
+}//End function
 
-void sendPicture(int device){
-            //Lets first find the row for the matrix     
-             int row = indexNumber[device]; //Find the row where the matix is located, ingrease pixel number by row number*100
+void sendPicture(int device){    
+              int row = indexNumber[device]; //Find the first pixel for the atom.
               WirePacker packer;
               packer.write('P');
               
-            //for(int i = row; i>=(row-240); i=i-60) //Find first row, 2nd row, 3rd row, 4th row, 5th row
             for(int i = row; i>=(row-240); i=i-60)
             { 
               //Do Nothing
@@ -251,8 +264,8 @@ void changePic(){
     char char_array[str_len];
       if(str_len>2){ 
       fileNames[actualPic].toCharArray(char_array, str_len);
-      readFile(SD, char_array); //readFile(SD, "/2.BMP"); 
-      for(int i = 0; i<16; i++){newPatern[i] = 1;} 
+      readFile(SD, char_array);   //Read the file from the SD
+      for(int i = 0; i<16; i++){newPatern[i] = 1;}    //Set the array to know a new picture is ready
       }
       actualPic+=1;
       if(actualPic == numberOfPics){actualPic = 0;}
